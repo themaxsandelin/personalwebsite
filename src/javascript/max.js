08063d70151d345306c0fef635ed8046ee6b7aac
Max Sandelin <themaxsandelin@gmail.com>
2017-04-22 07:19:07 +0200

    Removed velocity and added better scroll. Merged reset.css with max.css.

--- src/javascript/max.js
+++ src/javascript/max.js
@@ -1,10 +1,56 @@
+function jump (target, options) {
+  var start = window.pageYOffset;
+  var opt = {
+        duration: options.duration,
+        offset: options.offset || 0,
+        callback: options.callback,
+        easing: options.easing || easeInOutQuad
+      };
+  var distance = typeof target === 'string' ? opt.offset + document.querySelector(target).getBoundingClientRect().top : target;
+	var duration = typeof opt.duration === 'function' ? opt.duration(distance) : opt.duration;
+  var timeStart;
+  var timeElapsed;
+
+	requestAnimationFrame(function(time) { timeStart = time; loop(time); });
+
+	function loop (time) {
+		timeElapsed = time - timeStart;
+
+		window.scrollTo(0, opt.easing(timeElapsed, start, distance, duration));
+
+		if (timeElapsed < duration) {
+      requestAnimationFrame(loop);
+    } else {
+      end();
+    }
+	}
+
+	function end () {
+		window.scrollTo(0, start + distance);
+		if (typeof opt.callback === 'function') opt.callback();
+	}
+
+	function easeInOutQuad (t, b, c, d)  {
+		t /= d / 2;
+		if (t < 1) return c / 2 * t * t + b;
+		t--;
+		return -c / 2 * (t * (t - 2) - 1) + b;
+	}
+
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   // Loop through all menu items and add click event listeners.
   const menuItems = document.querySelectorAll('header .menu li');
   for (let i = 0; i < menuItems.length; i++) {
     menuItems[i].addEventListener('click', (e) => {
       const id = e.target.innerText.toLowerCase();
-      Velocity(document.getElementById(id), 'scroll', 600);
+      const position = document.getElementById(id).getBoundingClientRect();
+      const scrollTo = position.top - (document.body.scrollTop || document.documentElement.scrollTop);
+      
+      jump(scrollTo, {
+        duration: 600
+      });
     });
   }
 });
